bce2ae4ec56440f42ea6b3e53f001db0166cb4c2
zz.hacfox <hacfox@126.com>
2020-01-20 16:06:49 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='forpay_client',
-    version='1.0.10',
+    version='1.0.11',
     description=(
         'Python sdk for Forpay system'
     ),
