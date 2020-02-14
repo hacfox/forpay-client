e1adaff9046a5612d179c303e5fc2545e935c4ee
zz.hacfox <hacfox@126.com>
2020-02-14 12:19:33 +0800

    Configs can be obtained from environment configurations

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME = 'forpay_client'
-VERSION = '1.0.13'
+VERSION = '1.0.14'
 AUTHOR = 'bozimeile'
 EMAIL = 'zz.hacfox@gmail.com'
 URL = 'https://github.com/hacfox/forpay_client.git'
